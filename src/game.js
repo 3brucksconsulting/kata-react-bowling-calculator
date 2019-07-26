0ae43aef555df062b49cab895b6d7ecb341537ec
Ed Bruck <ed@3brucksconsulting.com>
2019-07-26 06:51:22 -0500

    Connected formattedFrames and scores to game

--- src/game.js
+++ src/game.js
@@ -4,6 +4,7 @@ import './bootstrap.min.css';
 import './game.css';
 
 const Game = props => {
+  const formattedFrames = calculator.formatFrames(props.frames);
   const scores = calculator.calculateScores(props.frames);
 
   return (
@@ -12,97 +13,93 @@ const Game = props => {
         <div className='frame-one'>
           <div className='frame-header'>1</div>
           <div className='frame-rolls'>
-            <div className='frame-first-roll'>6</div>
-            <div className='frame-second-roll'>3</div>
+            <div className='frame-first-roll'>{formattedFrames[0][0]}</div>
+            <div className='frame-second-roll'>{formattedFrames[0][1]}</div>
           </div>
-          <div className='frame-total'>9</div>
+          <div className='frame-total'>{scores[0]}</div>
         </div>
 
         <div className='frame'>
           <div className='frame-header'>2</div>
           <div className='frame-rolls'>
-            <div className='frame-first-roll'></div>
-            <div className='frame-second-roll'>X</div>
+            <div className='frame-first-roll'>{formattedFrames[1][0]}</div>
+            <div className='frame-second-roll'>{formattedFrames[1][1]}</div>
           </div>
-          <div className='frame-total'>29</div>
+          <div className='frame-total'>{scores[1]}</div>
         </div>
 
         <div className='frame'>
           <div className='frame-header'>3</div>
           <div className='frame-rolls'>
-            <div className='frame-first-roll'>9</div>
-            <div className='frame-second-roll'>/</div>
+            <div className='frame-first-roll'>{formattedFrames[2][0]}</div>
+            <div className='frame-second-roll'>{formattedFrames[2][1]}</div>
           </div>
-          <div className='frame-total'>47</div>
+          <div className='frame-total'>{scores[2]}</div>
         </div>
 
         <div className='frame'>
           <div className='frame-header'>4</div>
           <div className='frame-rolls'>
-            <div className='frame-first-roll'>8</div>
-            <div className='frame-second-roll'>/</div>
+            <div className='frame-first-roll'>{formattedFrames[3][0]}</div>
+            <div className='frame-second-roll'>{formattedFrames[3][1]}</div>
           </div>
-          <div className='frame-total'>66</div>
+          <div className='frame-total'>{scores[3]}</div>
         </div>
 
         <div className='frame'>
           <div className='frame-header'>5</div>
           <div className='frame-rolls'>
-            <div className='frame-first-roll'>9</div>
-            <div className='frame-second-roll'>-</div>
+            <div className='frame-first-roll'>{formattedFrames[4][0]}</div>
+            <div className='frame-second-roll'>{formattedFrames[4][1]}</div>
           </div>
-          <div className='frame-total'>75</div>
+          <div className='frame-total'>{scores[4]}</div>
         </div>
 
         <div className='frame'>
           <div className='frame-header'>6</div>
           <div className='frame-rolls'>
-            <div className='frame-first-roll'></div>
-            <div className='frame-second-roll'>X</div>
+            <div className='frame-first-roll'>{formattedFrames[5][0]}</div>
+            <div className='frame-second-roll'>{formattedFrames[5][1]}</div>
           </div>
-          <div className='frame-total'>103</div>
+          <div className='frame-total'>{scores[5]}</div>
         </div>
 
         <div className='frame'>
           <div className='frame-header'>7</div>
           <div className='frame-rolls'>
-            <div className='frame-first-roll'></div>
-            <div className='frame-second-roll'>X</div>
+            <div className='frame-first-roll'>{formattedFrames[6][0]}</div>
+            <div className='frame-second-roll'>{formattedFrames[6][1]}</div>
           </div>
-          <div className='frame-total'>121</div>
+          <div className='frame-total'>{scores[6]}</div>
         </div>
 
         <div className='frame'>
           <div className='frame-header'>8</div>
           <div className='frame-rolls'>
-            <div className='frame-first-roll'>8</div>
-            <div className='frame-second-roll'>-</div>
+            <div className='frame-first-roll'>{formattedFrames[7][0]}</div>
+            <div className='frame-second-roll'>{formattedFrames[7][1]}</div>
           </div>
-          <div className='frame-total'>129</div>
+          <div className='frame-total'>{scores[7]}</div>
         </div>
 
         <div className='frame'>
           <div className='frame-header'>9</div>
           <div className='frame-rolls'>
-            <div className='frame-first-roll'>9</div>
-            <div className='frame-second-roll'>/</div>
+            <div className='frame-first-roll'>{formattedFrames[8][0]}</div>
+            <div className='frame-second-roll'>{formattedFrames[8][1]}</div>
           </div>
-          <div className='frame-total'>149</div>
+          <div className='frame-total'>{scores[8]}</div>
         </div>
 
         <div className='frame'>
           <div className='frame-header'>10</div>
           <div className='frame-rolls'>
-            <div className='frame-tenth-first-roll'>X</div>
-            <div className='frame-tenth-second-roll'>X</div>
-            <div className='frame-tenth-third-roll'>X</div>
+            <div className='frame-tenth-first-roll'>{formattedFrames[9][0]}</div>
+            <div className='frame-tenth-second-roll'>{formattedFrames[9][1]}</div>
+            <div className='frame-tenth-third-roll'>{formattedFrames[9][2]}</div>
           </div>
-          <div className='frame-total'>179</div>
+          <div className='frame-total'>{scores[9]}</div>
         </div>
-        
-        {scores.map(score => (
-          <div key={score}>{score}</div>
-        ))}
       </div>
     </div>
   );
